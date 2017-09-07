00b16d3742bdd6e14d010ee4491ba984056ff880
MiguelBrito1086 <mbrito1086@gmail.com>
2017-09-07 13:37:49 -0400

    adding color and fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Yellow from '../yellow.jpg';
 
 export default class Header extends Component {
   render() {
     return (
       <div className='header'>
+        <img src={Yellow} alt="mug"/>
         <p className='hi'>
           Hi, my name is Miguel!
         </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,25 @@ export default class Projects extends Component {
   render() {
     return (
       <div className='projects'>
-        <img src={ RPS } alt="rps"/>
-        <p>
-          My name is Miguel! I have a love for the arts and for technology. I am now a potter turned developer and hoping that I can continue to create on the web and on the potter's wheel! Definitely loving how much there is to learn in the programming world.
-        </p>
+        <h1>Projects</h1>
+        <div className='project'>
+          <img src={ RPS } alt="rps"/>
+          <p>
+            My name is Miguel! I have a love for the arts and for technology. I am now a potter turned developer and hoping that I can continue to create on the web and on the potter's wheel! Definitely loving how much there is to learn in the programming world.
+          </p>
+        </div>
+        <div className='project'>
+          <img src={ RPS } alt="rps"/>
+          <p>
+            My name is Miguel! I have a love for the arts and for technology. I am now a potter turned developer and hoping that I can continue to create on the web and on the potter's wheel! Definitely loving how much there is to learn in the programming world.
+          </p>
+        </div>
+        <div className='project'>
+          <img src={ RPS } alt="rps"/>
+          <p>
+            My name is Miguel! I have a love for the arts and for technology. I am now a potter turned developer and hoping that I can continue to create on the web and on the potter's wheel! Definitely loving how much there is to learn in the programming world.
+          </p>
+        </div>
       </div>
     )
   }
