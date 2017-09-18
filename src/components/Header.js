0bf8fdb8eeae0a92843ec41deffb6f26beff1c56
MiguelBrito1086 <mbrito1086@gmail.com>
2017-09-18 12:29:44 -0400

    Media queries add for tablet and mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className='header'>
-        <img id='mug' src={Yellow} alt="mug"/>
+        {/* <img id='mug' src={Yellow} alt="mug"/> */}
         <p className='hi'>
           Hi, my name is Miguel!
         </p>
