eea398db8738fdff16655e1902608e8406fd3ae5
MiguelBrito1086 <mbrito1086@gmail.com>
2017-09-24 19:32:35 -0400

    about section moved

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,8 +7,8 @@ export default class NavBar extends Component {
 
     return (
       <nav className='navbar'>
-        <a href='#Projects' to='#Projects'>Projects</a>
         <a href='#AboutMe' to='#AboutMe'>About Me</a>
+        <a href='#Projects' to='#Projects'>Projects</a>
         <a href='#Contact' to='#Contact'>Contact Me</a>
       </nav>
     )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,14 @@ export default class Projects extends Component {
       <div className='projects' id='Projects'>
         <h1>Projects</h1>
         <div className='project'>
+          <a href="https://miguelbrito1086.github.io/e-commerce-mock/" target='_blank'>
+            <img src={ Ceramics } alt="Ceramics"/>
+            <p>
+              My Ceramics E-commerce site was also created using React JavaScript and implemented Link and NavLink's within the app to easily navigate through the pages and content. Very simple tool and highly functional for loading content from each link!
+            </p>
+          </a>
+        </div>
+        <div className='project'>
           <a href="https://miguelbrito1086.github.io/Rock-Paper-Scissors-Revamp/" target='_blank'>
             <img src={ RPS } alt="rps"/>
             <p>
@@ -21,14 +29,6 @@ export default class Projects extends Component {
             <img src={ Play } alt="PlayList"/>
             <p>
               The 'PlayList Cloud' project that I created was built using React JavaScript. It posts data to an external API using AJAX request. Simple app to post and share your favorite artists and songs!
-            </p>
-          </a>
-        </div>
-        <div className='project'>
-          <a href="https://miguelbrito1086.github.io/e-commerce-mock/" target='_blank'>
-            <img src={ Ceramics } alt="Ceramics"/>
-            <p>
-              My Ceramics E-commerce site was also created using React JavaScript and implemented Link and NavLink's within the app to easily navigate through the pages and content. Very simple tool and highly functional for loading content from each link!
             </p>
           </a>
         </div>
