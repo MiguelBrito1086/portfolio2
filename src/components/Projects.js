ee63ccbd1bd92a42a94902954b73794d46561f50
MiguelBrito1086 <mbrito1086@gmail.com>
2017-09-25 13:39:08 -0400

    added titles to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,25 +11,34 @@ export default class Projects extends Component {
         <div className='project'>
           <a href="https://miguelbrito1086.github.io/e-commerce-mock/" target='_blank'>
             <img src={ Ceramics } alt="Ceramics"/>
-            <p>
-              My Ceramics E-commerce site was also created using React JavaScript and implemented Link and NavLink's within the app to easily navigate through the pages and content. Very simple tool and highly functional for loading content from each link!
-            </p>
+            <div className='projSections'>
+              <p className='projTitle'>E-commerce Mock Site made with React</p>
+              <p className='projDescrip'>
+                My Ceramics E-commerce site was also created using React JavaScript and implemented Link and NavLink's within the app to easily navigate through the pages and content. Very simple tool and highly functional for loading content from each link!
+              </p>
+            </div>
           </a>
         </div>
         <div className='project'>
           <a href="https://miguelbrito1086.github.io/Rock-Paper-Scissors-Revamp/" target='_blank'>
             <img src={ RPS } alt="rps"/>
-            <p>
-              This was a simple Rock, Paper, Scissors game I created. At first I created it with an alert box that would pop up and ask you for your move. I decided to revamp it and styled it to be a fun retro style family favorite!
-            </p>
+            <div className='projSections'>
+              <p className='projTitle'>Rock, Paper Scissors Game made with HTML, CSS, & JavaScript</p>
+              <p className='projDescrip'>
+                This was a simple Rock, Paper, Scissors game I created. At first I created it with an alert box that would pop up and ask you for your move. I decided to revamp it and styled it to be a fun retro style family favorite!
+              </p>
+            </div>
           </a>
         </div>
         <div className='project'>
           <a href="https://miguelbrito1086.github.io/song-share/" target='_blank'>
             <img src={ Play } alt="PlayList"/>
-            <p>
-              The 'PlayList Cloud' project that I created was built using React JavaScript. It posts data to an external API using AJAX request. Simple app to post and share your favorite artists and songs!
-            </p>
+            <div className='projSections'>
+              <p className='projTitle'>'PlayList Cloud' made with React fetching to an API</p>
+              <p className='projDescrip'>
+                The 'PlayList Cloud' project that I created was built using React JavaScript. It posts data to an external API using AJAX request. Simple app to post and share your favorite artists and songs!
+              </p>
+            </div>
           </a>
         </div>
       </div>
